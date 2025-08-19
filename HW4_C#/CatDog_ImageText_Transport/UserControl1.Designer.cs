6c847d47d8197b726ff5ac53f10cd816d3b82899
Tomer Levi <tomerlevi252@gmail.com>
2025-08-19 23:53:32 +0300

    Add files via upload

---
+++ HW4_C#/CatDog_ImageText_Transport/UserControl1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace CatDog_ImageText_Transport
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // UserControl1
+            // 
+            AutoScaleDimensions = new SizeF(23F, 57F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Yellow;
+            Margin = new Padding(7, 5, 7, 5);
+            Name = "UserControl1";
+            Size = new Size(145, 360);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ HW4_C#/CatDog_ImageText_Transport/UserControl1.cs
@@ -0,0 +1,74 @@
+﻿
+
+
+
+
+
+
+
+namespace CatDog_ImageText_Transport
+{
+    public partial class UserControl1 : UserControl
+    {
+        static Random tempRand = new Random();
+        public Label[] arrLabels;
+        public UserControl1(int width, int MaxCounter, string FullEmpty)
+        {
+            InitializeComponent();
+            arrLabels = new Label[MaxCounter];
+            this.Width = width;
+            if (FullEmpty == "Full")
+            {
+                int lastX = 2;
+                for (int i = 0; i < MaxCounter; i++)
+                {
+                    int tempWidth = tempRand.Next(70, 90);
+                    if (tempWidth > width - lastX)
+                       break;
+                    int tempHeight= tempRand.Next(70, 90);
+
+                    arrLabels[i] = new Label();
+
+                    arrLabels[i].Location = new Point(lastX, 3);
+                    arrLabels[i].Size = new Size(tempWidth, tempHeight);
+                    lastX += arrLabels[i].Width + 2;
+                    arrLabels[i].Font = new Font("Arial", 12, FontStyle.Bold);
+                    arrLabels[i].ForeColor = Color.White;
+                    arrLabels[i].TextAlign = ContentAlignment.MiddleCenter;
+
+                    if (tempRand.Next(2) == 0)
+                        arrLabels[i].Name = "Cat";
+                    else
+                        arrLabels[i].Name = "Dog";
+
+                    switch (tempRand.Next(4))
+                    {
+                        case 0: arrLabels[i].BackColor = Color.Red; break;
+                        case 1: arrLabels[i].BackColor = Color.Green; break;
+                        case 2:
+                            arrLabels[i].BackColor = Color.White;
+                            arrLabels[i].Tag = "Red"; break;
+                        case 3:
+                            arrLabels[i].BackColor = Color.White;
+                            arrLabels[i].Tag = "Green"; break;
+                    }
+
+                    if (arrLabels[i].BackColor != Color.White)
+                        arrLabels[i].Text = arrLabels[i].Name;
+                    else
+                        arrLabels[i].Image = getImage(arrLabels[i].Size, "../../../" + arrLabels[i].Name + "_" + arrLabels[i].Tag + ".jpg");
+
+                    this.Controls.Add(arrLabels[i]);
+                }
+            }
+        }
+
+        private Image getImage(Size size, string path)
+        {
+            Image tempImage = Image.FromFile(path);
+            tempImage = (Image)new Bitmap(tempImage, size);
+            return tempImage;
+        }
+
+    }
+}
